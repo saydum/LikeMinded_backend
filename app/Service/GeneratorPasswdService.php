eb89314fdb28ed6afc3054ba8e77117d59257d8e
Saydum Halibekov <saydum.it5@gmail>
2022-01-09 18:37:09 +0300

    feat: created a service generator password

---
+++ app/Service/GeneratorPasswdService.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Services;
+
+class GeneratorPasswdService
+{
+    public static function make($length = 6): string
+    {
+        $password = '';
+        $arr = array(
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+            'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+            'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+            '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'
+        );
+
+        for ($i = 0; $i < $length; $i++) {
+            try {
+                $password .= $arr[random_int(0, count($arr) - 1)];
+            } catch (\Exception $e) {
+            }
+        }
+        return $password;
+    }
+}
