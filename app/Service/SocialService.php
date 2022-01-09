3ee8344cfaf4aff4f865e873c0db2a91bcb68e74
Saydum Halibekov <saydum.it5@gmail>
2022-01-09 18:34:18 +0300

    feat: created a service for authorization via vk.com

--- app/Service/SocialService.php
+++ app/Service/SocialService.php
@@ -2,7 +2,38 @@
 
 namespace App\Services;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
 class SocialService
 {
+    public function saveSocialData($user)
+    {
+        if ($user->getEmail() !== null and $user->getEmail() !== 0) {
+            $email = $user->getEmail();
+        } else {
+            $email = null;
+        }
+        $userId = $user->getId();
+        $name = $user->getName();
+        $avatar = $user->getAvatar();
 
+
+
+        $password = Hash::make('12345678');
+
+        $userModel = User::where('name', $name)->first();
+
+        if($userModel) {
+            return $userModel->fill(['name' => $name, 'avatar' => $avatar]);
+        }
+
+        return User::firstOrCreate([
+            'user_id' => $userId,
+            'name' => $name,
+            'email' => $email,
+            'avatar' => $avatar,
+            'password' => $password
+        ]);
+    }
 }
